7387303f7b11602c80611a5b96d9f31759a96f9e
Kevin Koech <koechkevin92@gmail.com>
2023-06-18 15:41:55 +0300

    PORT

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,6 +6,7 @@ module.exports = {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
         host: process.env.DB_HOST,
         dialect: "postgres",
     },
@@ -14,6 +15,7 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         dialect: "postgres",
     },
     production: {
@@ -21,6 +23,7 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         dialect: "postgres",
     },
 };
